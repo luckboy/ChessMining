b7c4c8005576b0a3c5c022fa9e4d28bcaf8cd7d1
Lukasz Szpakowski <luckboy@vp.pl>
2021-05-15 21:27:53 +0200

    Fixed license headers.

--- src/main/scala/pl/luckboy/chessmining/chess/package.scala
+++ src/main/scala/pl/luckboy/chessmining/chess/package.scala
@@ -12,8 +12,9 @@
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
  *
- * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ * You should have received a copy of the GNU Lesser General Public
+ * License and the GNU General Public License along with this program.
+ * If not, see <http://www.gnu.org/licenses/>.
  */
 package pl.luckboy.chessmining
 
