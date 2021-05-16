c745cb72eb9e81337b2bffdcd3e7b0c9143244cb
Lukasz Szpakowski <luckboy@vp.pl>
2021-05-16 07:50:22 +0200

    Again fixed license headers.

--- src/main/scala/pl/luckboy/chessmining/chess/TimeControl.scala
+++ src/main/scala/pl/luckboy/chessmining/chess/TimeControl.scala
@@ -2,18 +2,18 @@
  * Chess Mining - Library to data mining for chess games.
  * Copyright (C) 2021 Łukasz Szpakowski
  *
- * This program is free software: you can redistribute it and/or modify
+ * This library is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Lesser General Public License as
  * published by the Free Software Foundation, either version 3 of the
  * License, or (at your option) any later version.
  *
- * This program is distributed in the hope that it will be useful,
+ * This library is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
  *
  * You should have received a copy of the GNU Lesser General Public
- * License and the GNU General Public License along with this program.
+ * License and the GNU General Public License along with this library.
  * If not, see <http://www.gnu.org/licenses/>.
  */
 package pl.luckboy.chessmining.chess
