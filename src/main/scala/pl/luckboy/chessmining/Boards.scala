21ba5cc0bd6639583bac96df68e914011c2037f3
Lukasz Szpakowski <luckboy@vp.pl>
2021-05-24 11:22:32 +0200

    Added objects of moves and objects of boards.

---
+++ src/main/scala/pl/luckboy/chessmining/Boards.scala
@@ -0,0 +1,26 @@
+/*
+ * Chess Mining - Library to data mining for chess games.
+ * Copyright (C) 2021 Łukasz Szpakowski
+ *
+ * This library is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received copies of the GNU Lesser General Public
+ * License and the GNU General Public License along with this library.
+ * If not, see <http://www.gnu.org/licenses/>.
+ */
+package pl.luckboy.chessmining
+import pl.luckboy.chessmining.chess._
+import pl.luckboy.chessmining.iterator._
+
+object Boards
+{
+  def fromGame(game: Game) = new BoardIteratorWithoutVariations(game)
+}
